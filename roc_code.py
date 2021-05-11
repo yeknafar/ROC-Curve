e4d632a11324b3015d60289d5b0551f020c37be4
hamed <hamed74ghasemi@gmail.com>
2021-05-11 18:49:02 +0430

    roc and auc on keras added

--- roc_code.py
+++ roc_code.py
@@ -1,39 +1,44 @@
 # roc curve and auc
 from sklearn.datasets import make_classification
-from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import roc_curve
-from sklearn.metrics import roc_auc_score
+from sklearn.metrics import roc_curve, roc_auc_score, confusion_matrix
 from matplotlib import pyplot
-# generate 2 class dataset
+from tensorflow.keras import models
+from tensorflow.keras import layers
+from tensorflow.keras.utils import to_categorical
+import numpy as np
+import matplotlib.pyplot as plt
+
 X, y = make_classification(n_samples=1000, n_classes=2, random_state=1)
-# split into train/test sets
 trainX, testX, trainy, testy = train_test_split(X, y, test_size=0.5, random_state=2)
-# generate a no skill prediction (majority class)
-ns_probs = [0 for _ in range(len(testy))]
-# fit a model
-model = LogisticRegression(solver='lbfgs')
-model.fit(trainX, trainy)
-# predict probabilities
-lr_probs = model.predict_proba(testX)
-# keep probabilities for the positive outcome only
-lr_probs = lr_probs[:, 1]
-# calculate scores
-ns_auc = roc_auc_score(testy, ns_probs)
-lr_auc = roc_auc_score(testy, lr_probs)
-# summarize scores
-print('No Skill: ROC AUC=%.3f' % (ns_auc))
-print('Logistic: ROC AUC=%.3f' % (lr_auc))
-# calculate roc curves
-ns_fpr, ns_tpr, _ = roc_curve(testy, ns_probs)
-lr_fpr, lr_tpr, _ = roc_curve(testy, lr_probs)
-# plot the roc curve for the model
-pyplot.plot(ns_fpr, ns_tpr, linestyle='--', label='No Skill')
-pyplot.plot(lr_fpr, lr_tpr, marker='.', label='Logistic')
-# axis labels
-pyplot.xlabel('False Positive Rate')
-pyplot.ylabel('True Positive Rate')
-# show the legend
-pyplot.legend()
-# show the plot
-pyplot.show()
+
+ns_probs = np.zeros(shape = len(testy))
+
+inputs = layers.Input(shape = (20,))
+x = layers.Dense(20, activation = "relu")(inputs)
+x = layers.Dense(10, activation = "relu")(x)
+out = layers.Dense(1, activation = "sigmoid")(x)
+
+net = models.Model(inputs, out)
+
+net.compile(optimizer=  "sgd",
+              loss = "binary_crossentropy",
+              metrics = ["accuracy"])
+
+h = net.fit(trainX, trainy, batch_size=32, epochs=15)
+
+preds = net.predict(testX).ravel()
+
+auc_mlp = roc_auc_score(testy, preds)
+auc_random = roc_auc_score(testy, ns_probs)
+print(np.zeros(shape = testy.shape))
+print("auc MLP: ", auc_mlp)
+print("auc random: ", auc_random)
+
+random_fpr, random_tpr, _ = roc_curve(testy,ns_probs)
+mlp_fpr, mlp_tpr, _ = roc_curve(testy, preds)
+
+plt.plot(random_fpr, random_tpr, linestyle="-", label = "random")
+plt.plot(mlp_fpr, mlp_tpr, linestyle="--", label = "mlp")
+plt.legend()
+plt.show()
